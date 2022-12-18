64016bc8a3e38cb10818e50b372b80aac6fd965c
KKaiyrbekov <han_umag@mail.com>
2022-12-18 23:57:54 +0600

    use random

--- src/test/java/quru/qa/ExampleTest.java
+++ src/test/java/quru/qa/ExampleTest.java
@@ -2,16 +2,17 @@ package quru.qa;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 public class ExampleTest {
 
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
